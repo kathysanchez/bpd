3f714de3978c2532bc06464b98e938a850231aeb
Kathy Sanchez <sanchekathy@gmail.com>
2025-05-06 20:30:10 -0400

    gitignore

--- bpd.py
+++ bpd.py
@@ -2,6 +2,7 @@
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
 import seaborn as sns
 import folium
 from shapely.geometry import Point
@@ -16,6 +17,7 @@ print(Path.cwd())
 
 gdf = gpd.read_file("BPD_Arrests.geojson") # Source: Open Baltimore. Date updated 3/10/2025 Date created 3/10/2021
 print(gdf.head())
+gdf.columns.tolist()
 
 # Check current CRS
 print(gdf.crs) # Check coordinate reference sys
@@ -32,14 +34,17 @@ gdf_nonmissing = gdf.dropna(subset=['latitude', 'longitude'])
 print(gdf_nonmissing.head())
 
 gdf_nonmissing.columns.tolist()
-gdf_nonmissing.ArrestDateTime.value
-
-# TODO subset df
-
+gdf_nonmissing.ArrestDateTime.value_counts()
+gdf_nonmissing.dtypes
 
 
+# Subset 2024 only
 
-map_center = [gdf.geometry.y.mean(), gdf.geometry.x.mean()]
+gdf_nonmissing = gdf_nonmissing[gdf_nonmissing['ArrestDateTime'].dt.year == 2024]
+
+# Make map
+
+map_center = [gdf_nonmissing.geometry.y.mean(), gdf_nonmissing.geometry.x.mean()]
 m = folium.Map(location=map_center, zoom_start=12)
 
 # Add crime points to the map using apply()
@@ -49,7 +54,7 @@ def add_crime_marker(row):
     ).add_to(m)
 
 # Apply the function to each row of the GeoDataFrame
-gdf.apply(add_crime_marker, axis=1)
+gdf_nonmissing.apply(add_crime_marker, axis=1)
 # Save to an HTML file
 m.save("map_arrests.html")
 webbrowser.open("map_arrests.html")
